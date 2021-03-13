b0ae74dddc040c318a7bd644318ce6ddc44f6e95
MangoAbricosik <gg.tajik@yandex.ru>
2021-03-13 22:52:46 +0300

    Q

--- PR_IT-6_2/Form1.Designer.cs
+++ PR_IT-6_2/Form1.Designer.cs
@@ -32,11 +32,11 @@ namespace PR_IT_6_2
       this.dataGridView1 = new System.Windows.Forms.DataGridView();
       this.button1 = new System.Windows.Forms.Button();
       this.groupBox1 = new System.Windows.Forms.GroupBox();
-      this.radioButton1 = new System.Windows.Forms.RadioButton();
-      this.radioButton2 = new System.Windows.Forms.RadioButton();
-      this.radioButton3 = new System.Windows.Forms.RadioButton();
-      this.radioButton4 = new System.Windows.Forms.RadioButton();
       this.radioButton5 = new System.Windows.Forms.RadioButton();
+      this.radioButton4 = new System.Windows.Forms.RadioButton();
+      this.radioButton3 = new System.Windows.Forms.RadioButton();
+      this.radioButton2 = new System.Windows.Forms.RadioButton();
+      this.radioButton1 = new System.Windows.Forms.RadioButton();
       this.label1 = new System.Windows.Forms.Label();
       this.button2 = new System.Windows.Forms.Button();
       ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -53,11 +53,11 @@ namespace PR_IT_6_2
       // 
       // button1
       // 
-      this.button1.Location = new System.Drawing.Point(225, 361);
+      this.button1.Location = new System.Drawing.Point(218, 366);
       this.button1.Name = "button1";
       this.button1.Size = new System.Drawing.Size(75, 23);
       this.button1.TabIndex = 1;
-      this.button1.Text = "button1";
+      this.button1.Text = "Начать";
       this.button1.UseVisualStyleBackColor = true;
       this.button1.Click += new System.EventHandler(this.button1_Click);
       // 
@@ -75,65 +75,65 @@ namespace PR_IT_6_2
       this.groupBox1.TabStop = false;
       this.groupBox1.Text = "groupBox1";
       // 
-      // radioButton1
+      // radioButton5
       // 
-      this.radioButton1.AutoSize = true;
-      this.radioButton1.Location = new System.Drawing.Point(27, 16);
-      this.radioButton1.Name = "radioButton1";
-      this.radioButton1.Size = new System.Drawing.Size(85, 17);
-      this.radioButton1.TabIndex = 0;
-      this.radioButton1.TabStop = true;
-      this.radioButton1.Text = "radioButton1";
-      this.radioButton1.UseVisualStyleBackColor = true;
-      // 
-      // radioButton2
-      // 
-      this.radioButton2.AutoSize = true;
-      this.radioButton2.Location = new System.Drawing.Point(27, 49);
-      this.radioButton2.Name = "radioButton2";
-      this.radioButton2.Size = new System.Drawing.Size(85, 17);
-      this.radioButton2.TabIndex = 1;
-      this.radioButton2.TabStop = true;
-      this.radioButton2.Text = "radioButton2";
-      this.radioButton2.UseVisualStyleBackColor = true;
-      // 
-      // radioButton3
-      // 
-      this.radioButton3.AutoSize = true;
-      this.radioButton3.Location = new System.Drawing.Point(27, 84);
-      this.radioButton3.Name = "radioButton3";
-      this.radioButton3.Size = new System.Drawing.Size(85, 17);
-      this.radioButton3.TabIndex = 2;
-      this.radioButton3.TabStop = true;
-      this.radioButton3.Text = "radioButton3";
-      this.radioButton3.UseVisualStyleBackColor = true;
+      this.radioButton5.AutoSize = true;
+      this.radioButton5.Location = new System.Drawing.Point(27, 178);
+      this.radioButton5.Name = "radioButton5";
+      this.radioButton5.Size = new System.Drawing.Size(77, 17);
+      this.radioButton5.TabIndex = 4;
+      this.radioButton5.TabStop = true;
+      this.radioButton5.Text = "Задание 6";
+      this.radioButton5.UseVisualStyleBackColor = true;
       // 
       // radioButton4
       // 
       this.radioButton4.AutoSize = true;
       this.radioButton4.Location = new System.Drawing.Point(27, 129);
       this.radioButton4.Name = "radioButton4";
-      this.radioButton4.Size = new System.Drawing.Size(85, 17);
+      this.radioButton4.Size = new System.Drawing.Size(77, 17);
       this.radioButton4.TabIndex = 3;
       this.radioButton4.TabStop = true;
-      this.radioButton4.Text = "radioButton4";
+      this.radioButton4.Text = "Задание 5";
       this.radioButton4.UseVisualStyleBackColor = true;
       // 
-      // radioButton5
+      // radioButton3
       // 
-      this.radioButton5.AutoSize = true;
-      this.radioButton5.Location = new System.Drawing.Point(27, 178);
-      this.radioButton5.Name = "radioButton5";
-      this.radioButton5.Size = new System.Drawing.Size(85, 17);
-      this.radioButton5.TabIndex = 4;
-      this.radioButton5.TabStop = true;
-      this.radioButton5.Text = "radioButton5";
-      this.radioButton5.UseVisualStyleBackColor = true;
+      this.radioButton3.AutoSize = true;
+      this.radioButton3.Location = new System.Drawing.Point(27, 84);
+      this.radioButton3.Name = "radioButton3";
+      this.radioButton3.Size = new System.Drawing.Size(77, 17);
+      this.radioButton3.TabIndex = 2;
+      this.radioButton3.TabStop = true;
+      this.radioButton3.Text = "Задание 4";
+      this.radioButton3.UseVisualStyleBackColor = true;
+      // 
+      // radioButton2
+      // 
+      this.radioButton2.AutoSize = true;
+      this.radioButton2.Location = new System.Drawing.Point(27, 49);
+      this.radioButton2.Name = "radioButton2";
+      this.radioButton2.Size = new System.Drawing.Size(77, 17);
+      this.radioButton2.TabIndex = 1;
+      this.radioButton2.TabStop = true;
+      this.radioButton2.Text = "Задание 3";
+      this.radioButton2.UseVisualStyleBackColor = true;
+      // 
+      // radioButton1
+      // 
+      this.radioButton1.AutoSize = true;
+      this.radioButton1.Location = new System.Drawing.Point(27, 16);
+      this.radioButton1.Name = "radioButton1";
+      this.radioButton1.Size = new System.Drawing.Size(77, 17);
+      this.radioButton1.TabIndex = 0;
+      this.radioButton1.TabStop = true;
+      this.radioButton1.Text = "Задание 2";
+      this.radioButton1.UseVisualStyleBackColor = true;
       // 
       // label1
       // 
       this.label1.AutoSize = true;
-      this.label1.Location = new System.Drawing.Point(506, 350);
+      this.label1.Location = new System.Drawing.Point(434, 331);
       this.label1.Name = "label1";
       this.label1.Size = new System.Drawing.Size(35, 13);
       this.label1.TabIndex = 5;
@@ -143,9 +143,9 @@ namespace PR_IT_6_2
       // 
       this.button2.Location = new System.Drawing.Point(437, 366);
       this.button2.Name = "button2";
-      this.button2.Size = new System.Drawing.Size(75, 23);
+      this.button2.Size = new System.Drawing.Size(116, 23);
       this.button2.TabIndex = 6;
-      this.button2.Text = "button2";
+      this.button2.Text = "Поменять местами";
       this.button2.UseVisualStyleBackColor = true;
       this.button2.Click += new System.EventHandler(this.button2_Click);
       // 
